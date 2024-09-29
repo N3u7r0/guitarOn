553671dfaeb07728d48fcc9a44c55b78754e3916
Brian Sabatini <brian_sabatini1993@hotmail.com>
2024-09-28 22:50:54 -0300

    EntregaFinal Brian Sabatini, coder 60030

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 export const Home = () => {
   return (
-    <Heading itemType={"h2"} size={"xl"} _hover={{ color: "red.500" }}>
-      Home
-    </Heading>
+    <Box textAlign={"center"} py={10}>
+      <Heading itemType={"h2"} size={"xl"} _hover={{ color: "red.500" }}>
+        Home
+      </Heading>
+      <br />
+      <Text>
+        profe las cosas siguen en poroductos,el trabajo cumple con lo que pide{" "}
+        <br />
+        la ultima entrega, pero me gustaria decorar y terminan el home, ofertas
+        <br />
+        y nosotros (mas otros detalles como el footer. si puedo completo esas
+        cosas.
+        <br />
+        saludos!!!
+      </Text>
+    </Box>
   );
 };
